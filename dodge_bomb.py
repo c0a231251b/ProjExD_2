7ac4551fc473e85c0a71ffbdf579edd3b8afd241
haruta <c0a23125b@edu.teu.ac.jp>
2024-11-26 16:23:24 +0900

    演習課題2 爆弾が拡大と加速する

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -30,8 +30,7 @@ def check_bound(rct:pg.Rect) -> tuple[bool,bool]:
         tate=False
     return yoko,tate
 
-#追加部分
-
+#演習課題1
 def gameover(screen: pg.Surface) -> None:
     """
     ゲームオーバー時に，半透明の黒い画面上に「Game Over」と表示し，
@@ -58,6 +57,22 @@ def gameover(screen: pg.Surface) -> None:
     pg.display.update()  # 描画内容を更新
     time.sleep(5)  # 5秒間表示
 
+#演習課題2
+def init_bb_imgs() -> tuple[list[pg.Surface], list[int]]:
+    """
+    サイズの異なる爆弾Surfaceを要素としたリストと加速度リストを返す
+    """
+    bb_imgs = []  # 爆弾の画像リスト
+    for r in range(1, 11):
+        bb_img = pg.Surface((20 * r, 20 * r), pg.SRCALPHA)  # サイズ変更
+        pg.draw.circle(bb_img, (255, 0, 0), (10 * r, 10 * r), 10 * r)  # 爆弾描画
+        bb_imgs.append(bb_img)
+    accs = [a for a in range(1, 11)]  # 加速度リスト
+    return bb_imgs, accs
+
+        
+
+
 
 
 def main():
@@ -72,7 +87,9 @@ def main():
     bb_img.set_colorkey((0,0,0)) #四隅の黒を透過させる
     bb_rct = bb_img.get_rect() #爆弾円rectの抽出
     bb_rct.center=random.randint(0,WIDTH),random.randint(0,HEIGHT)
-    vx,vy=+5,+5 #爆弾速度ベクトル
+    bb_imgs, bb_accs = init_bb_imgs()  # 爆弾画像と加速度リストを取得
+    bb_img = bb_imgs[0]  # 初期爆弾画像
+    vx,vy=+2,+2 #爆弾速度ベクトル 確認の際に臨機に変更
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -95,7 +112,17 @@ def main():
         if check_bound(kk_rct)!=(True,True):
             kk_rct.move_ip(-sum_mv[0],-sum_mv[1])
         screen.blit(kk_img, kk_rct)
+
+             # 爆弾のサイズと加速の更新
+        idx = min(tmr // 500, 9)  # 時間に応じて段階を選択（最大9）
+        avx = vx * bb_accs[idx]  # 加速度適用
+        avy = vy * bb_accs[idx]
+        bb_img = bb_imgs[idx]  # サイズ更新
+
+
+
         bb_rct.move_ip(vx,vy) #爆弾動く
+        bb_rct.move_ip(avx,avy)
         yoko,tate=check_bound(bb_rct)
         if not yoko:
             vx*=-1
